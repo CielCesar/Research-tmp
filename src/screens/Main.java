4ef12ed5ad0b8423448bbddae96351122f5adf45
Ciel Slytherin <cielslytherin@CieldeMacBook-Air.local>
2017-05-11 22:42:59 -0700

    Javafx sample

---
+++ src/screens/Main.java
@@ -0,0 +1,36 @@
+package screens;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.application.*;
+
+public class Main extends Application{
+
+	private Stage primaryStage;
+	private BorderPane mainLayout;
+	
+	public void start(Stage primaryStage) throws IOException {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("TODO");
+		showMainView();
+	}
+	
+	private void showMainView() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("view/Screen1.fxml"));
+		mainLayout = loader.load();
+		Scene scene = new Scene(mainLayout);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+}
